1329ed2de5ba85629e4aec6b9bd50be21762afeb
azeelleeza <leezaluncheon@gmail.com>
2018-05-22 10:39:29 -0400

    done

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,14 @@
 function insertionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let i = 1;
+  while (i < arr.length) {
+    let j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      let temp1 = arr[j];
+      let temp2 = arr[j - 1];
+      arr[j] = temp2;
+      arr[j - 1] = temp1;
+      j = j - 1;
+    }
+    i = 1 + i;
+  }
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,27 @@
+// function selectionSort(arr) {
+//   let halfArr = arr;
+//   let half = halfArr.length / 2;
+//   first = halfArr.slice(0, half);
+//   second = halfArr.slice(half, halfArr.length);
+//   first.sort((a, b) => a > b);
+//   second.sort((a, b) => a > b);
+//   insertionSort(halfArr);
+//   return [...first, ...second];
+// }
+
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (j = 0; j < arr.length; j++) {
+    let min = j;
+    for (i = j + 1; i < arr.length; i++) {
+      if (arr[i] < arr[min]) {
+        min = i;
+      }
+    }
+    if (min != j) {
+      let temp1 = arr[min];
+      let temp2 = arr[j];
+      arr[min] = temp2;
+      arr[j] = temp1;
+    }
+  }
 }
